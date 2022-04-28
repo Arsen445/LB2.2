0a9728e614723e4b5e48143d261ce2084b7ad764
Arsen445 <arsen454545454@gmail.com>
2022-04-28 20:26:38 +0300

    added prog

---
+++ ind/ind2.py
@@ -0,0 +1,12 @@
+"""
+Определить, есть ли среди трёх заданных чисел чётные.
+"""
+a = int(input('add 1st num: '))
+b = int(input('add 2nd num: '))
+c = int(input('add 3rd num: '))
+if (a % 2 == 0 or
+    b % 2 == 0 or
+    c % 2 == 0):
+    print('one or more of three added nums is even')
+else:
+    print('no one of three added nums is even')
