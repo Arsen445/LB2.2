771c786dd663b716a7e42497f25161a7260b2166
Arsen445 <arsen454545454@gmail.com>
2022-05-25 23:03:52 +0300

    fixxx

--- ind/ind2.py
+++ ind/ind2.py
@@ -1,12 +1,11 @@
-"""
-Определить, есть ли среди трёх заданных чисел чётные.
-"""
-a = int(input('add 1st num: '))
-b = int(input('add 2nd num: '))
-c = int(input('add 3rd num: '))
-if (a % 2 == 0 or
-    b % 2 == 0 or
-    c % 2 == 0):
-    print('one or more of three added nums is even')
-else:
-    print('no one of three added nums is even')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    a = int(input('add 1st num: '))
+    b = int(input('add 2nd num: '))
+    c = int(input('add 3rd num: '))
+    if a % 2 == 0 or b % 2 == 0 or c % 2 == 0:
+        print('one or more of three added nums is even')
+    else:
+        print('no one of three added nums is even')
--- ind/ind3.py
+++ ind/ind3.py
@@ -1,8 +1,9 @@
-"""
-Найти сумму целых положительных чисел, больших 20, меньших 100 и кратных 3.
-"""
-sum = 0
-for i in range(20, 100):
-    if i % 3 == 0:
-        sum += i
-print(sum)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    sum = 0
+    for i in range(20, 100):
+        if i % 3 == 0:
+            sum = sum + i
+    print(sum)
